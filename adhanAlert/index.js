4143c1c1b4f4daff828d6f9ad6e8526bafc9961e
Gulzey <79972723+Gulzey@users.noreply.github.com>
2024-07-14 11:55:25 +0100

    Upload

---
+++ adhanAlert/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the index.html file at the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
